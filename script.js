2cf7aae6efb87f78fa9134dce18fa1007298f39e
Denis2982 <denisdenis2982@gmail.com>
2023-02-20 14:51:32 +0200

    adding a burger menu

---
+++ script.js
@@ -0,0 +1,6 @@
+function navburger() {
+    var listBurger = document.getElementById('listBurger');
+    var menuBurger = document.getElementById('menuBurger');
+    listBurger.classList.toggle('active');
+    menuBurger.classList.toggle('active');
+}
